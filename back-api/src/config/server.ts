dec0f896e6c0ff0848f25d28ef8fc238d398ed40
Cosme Lorim <cosme.lorim@gmail.com>
2021-05-01 17:13:12 -0300

    Adiciona middlewares de logs

--- back-api/src/config/server.ts
+++ back-api/src/config/server.ts
@@ -1,10 +1,18 @@
 import { ApolloServer } from 'apollo-server-express'
+import * as morgan from 'morgan'
 import * as express from 'express'
 import * as cors from 'cors'
 
 import schema from '../schema/schema'
 
+import middlewareLogs from '../middleware/logs'
+
 const app = express()
+
+/** Logs mais genéricos da requisição */
+app.use(morgan(':remote-addr - :remote-user [:date] ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent" :response-time ms'))
+/** Middleware de log mais personalizável */
+app.use(middlewareLogs)
 app.use(cors())
 
 const server = new ApolloServer({
