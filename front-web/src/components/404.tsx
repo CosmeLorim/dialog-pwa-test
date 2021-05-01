2d61409ef3b1861b5e606247c5ba701b9fd59f22
Cosme Lorim <cosme.lorim@gmail.com>
2021-05-01 12:25:25 -0300

    Adiciona componente para erro 404

---
+++ front-web/src/components/404.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <div>
+      <img src='https://neilpatel.com/wp-content/uploads/2019/05/ilustracao-panda-com-problema-error-404-not-found.jpeg' alt='Erro 404' />
+    </div>
+  )
+}
+
+export default NotFound
