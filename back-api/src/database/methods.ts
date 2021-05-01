79965d28535e3c1a694e688cd44e44ad177925cd
Cosme Lorim <cosme.lorim@gmail.com>
2021-05-01 12:39:02 -0300

    Adiciona busca por regex na função db.find()

--- back-api/src/database/methods.ts
+++ back-api/src/database/methods.ts
@@ -1,11 +1,17 @@
-import { User } from '../schema/types'
 import _db from './_db'
+import {
+  User,
+} from '../schema/types'
 
 export const find = ({
   search,
 }: {
   search: string,
 }): User[] => _db.filter((d: User) => {
+  const regex = new RegExp(search.toLowerCase().split(' ').join('|'))
+
+  return regex.test(d.name.toLocaleLowerCase())
+})
   /** TODO: adicionar consulta com regex */
   /** TODO: adicionar consulta em mais campos */
   const nameIsEqual = d.name === search
