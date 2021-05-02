1d92c0d475037c55586fb50748014478c06e55dd
Cosme Lorim <cosme.lorim@gmail.com>
2021-05-02 19:54:26 -0300

    Adiciona a estilização global

--- front-web/src/global.ts
+++ front-web/src/global.ts
@@ -1,5 +1,18 @@
 import { createGlobalStyle } from 'styled-components';
 
 export default createGlobalStyle`
-
+  html, body {
+    font-family: "Inter", sans-serif;
+    margin: 0px;
+    padding: 0px 10px;
+    box-sizing: border-box;
+    background: #fff;
+  }
+  a {
+    text-decoration: none;
+  }
+  h1 {
+    margin: 16px 16px;
+    font-size: bold;
+  }
 `;
