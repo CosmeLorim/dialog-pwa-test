04ad37ab045103214566634e495d67ab84aefb37
Cosme Lorim <cosme.lorim@gmail.com>
2021-05-02 19:54:10 -0300

    Estiliza o cabeçalho

--- front-web/src/components/header.tsx
+++ front-web/src/components/header.tsx
@@ -1,15 +1,33 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import style from 'styled-components'
 
 import InputSearch from './inputSearch'
+
+const Grid = style.div`
+  display: flex;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  align-items: center;
+
+  .col2 {
+    grid-column-start: 2;
+    grid-column-end: 4;
+  }
+`
 
 const Header = ({ search }: { search: Function }) => {
   return (
     <header className="App-header">
-      <Link to='/'>
-        <h1>MySocial</h1>
-      </Link>
-      <InputSearch msForSearch={1000} search={search} />
+      <Grid>
+        <div className='col1'>
+          <Link to='/'>
+            <h1>MySocial</h1>
+          </Link>
+        </div>
+        <div className='col2'>
+          <InputSearch msForSearch={1000} search={search} />
+        </div>
+      </Grid>
     </header>
   )
 }
