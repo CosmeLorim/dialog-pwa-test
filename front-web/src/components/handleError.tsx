ec6a2131bb93beb5708b5601f40bf77796cbf11f
Cosme Lorim <cosme.lorim@gmail.com>
2021-05-03 04:30:53 -0300

    Adiciona tratativa para erro de rede no componente

--- front-web/src/components/handleError.tsx
+++ front-web/src/components/handleError.tsx
@@ -1,6 +1,13 @@
 import React from 'react'
+import { ApolloError } from '@apollo/client'
 
-const HandleError = () => {
+const HandleError = ({ error }: { error?: ApolloError }) => {
+  if (error?.networkError) {
+    return (
+      <p>Você está desconectado, verique usa conexão</p>
+    )
+  }
+
   return (
     <div>
       <p>Algo de errado não está certo... Não foi possível carregar a página</p>
