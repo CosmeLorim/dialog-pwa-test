466bc073b6c293ce7cbccfaa8ef33cdb23bc1bb9
Cosme Lorim <cosme.lorim@gmail.com>
2021-05-02 23:06:23 -0300

    Aplica estilização no componente de perfil

--- front-web/src/components/friendProfile.tsx
+++ front-web/src/components/friendProfile.tsx
@@ -1,21 +1,53 @@
 import React from 'react'
+import style from 'styled-components'
+import { Link } from 'react-router-dom'
 
 import { People, User } from '../types'
 
+const Container = style.div`
+  padding: 20px;
+  border: solid 1px;
+  border-radius: 10px;
+  border-color: #000;
+
+  img {
+    padding-bottom: 15px;
+  }
+`
+
 const FriendProfile = ({
   user,
+  to,
 }: {
   user: User | People,
+  to?: string,
 }) => {
   return (
-    <div>
-      <img src={user.picture ?? ''} alt={`Foto de perfil de ${user.name ?? ''}`}/>
+    <Container>
+      {
+        to ? (
+          <Link to={to}>
+            <img
+              width='100%'
+              src={user.picture ?? ''}
+              alt={`Foto de perfil de ${user.name ?? ''}`}
+            />
+          </Link>
+        )
+          : (
+            <img
+              width='100%'
+              src={user.picture ?? ''}
+              alt={`Foto de perfil de ${user.name ?? ''}`}
+            />
+          )
+      }
       <p>name: {user.name ?? ''}</p>
       <p>age: {user.age ?? ''}</p>
       <p>eyeColor: {user.eyeColor ?? ''}</p>
       <p>company: {user.company ?? ''}</p>
       <p>email: {user.email ?? ''}</p>
-    </div>
+    </Container>
   )
 }
 
